d10ff81d58d2c6ff96ec68a7a487adb5cd199cf2
Filipe Pereira <f29.pereira@gmail.com>
2026-09-07 20:53:06 +0100

    feat: Update auth.utils email and password validation functions
    - Update translations
    - Add `validate` property to the passwordValidation function

--- src/features/auth/components/util/auth.utils.ts
+++ src/features/auth/components/util/auth.utils.ts
@@ -1,6 +1,7 @@
 import type { TFunction } from "i18next";
 import type { RegisterOptions } from "react-hook-form";
 import type { AuthenticationFields } from "../types/auth.types";
+import { getPasswordStatus } from "../shared/PasswordField/PasswordRules/PasswordRules.utils";
 
 /**
  * Returns the React Hook Form validation for the email field
@@ -10,29 +11,28 @@ export const emailValidation = (
   t: TFunction<"translation", undefined>,
 ): RegisterOptions<AuthenticationFields, "email"> => {
   return {
-    required: t("forms.signUp.auth-step.errorMessages.required"),
+    required: t("forms.generic-errorMessages.required"),
     pattern: {
       value: /^[\w-.]+@(?:[\w-]+\.)+[\w-]{2,4}$/,
-      message: t("forms.signUp.auth-step.errorMessages.invalid"),
+      message: t("forms.generic-errorMessages.invalid"),
     },
   };
 };
 
 /**
  * Returns the React Hook Form validation for the password field
- * @param t error messages translation
+ * @param t        error messages translation
+ * @param password password field
  */
 export const passwordValidation = (
   t: TFunction<"translation", undefined>,
+  password: string,
 ): RegisterOptions<AuthenticationFields, "password"> => {
   return {
-    required: t("forms.signUp.auth-step.errorMessages.required"),
-    /**
-     * Validate if all password rules are OK
-     */
+    required: t("forms.generic-errorMessages.required"),
+    validate: async () => {
+      const status = await getPasswordStatus(password);
+      return status.isValid || t("forms.generic-errorMessages.invalid");
+    },
   };
 };
-
-/**
- * TO DO: Add password rules
- */

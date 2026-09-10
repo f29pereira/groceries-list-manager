ac3f7f0314937b7fe5c9e24ab4914c8c49045a7f
Filipe Pereira <f29.pereira@gmail.com>
2026-09-10 17:00:01 +0100

    feat: Update AuthStep styles, onSubmit. Add form error message
    - Update onSubmit function: Add Firebase createUserWithEmailAndPassword
    - Update styles: For viewports >= 1024px place the main title on the left side
    - Integrate the FormErrorMessage component

--- src/features/auth/components/SignUpForm/AuthStep/AuthStep.tsx
+++ src/features/auth/components/SignUpForm/AuthStep/AuthStep.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useForm, FormProvider } from "react-hook-form";
 import type { AuthStepProps } from "./AuthStep.types";
 import type { AuthenticationFields } from "../../types/auth.types";
+import {
+  signUpEmailValidation,
+  signUpPasswordValidation,
+  getCreateUserErrorMessage,
+} from "./AuthStep.utils";
+import { useAuth } from "@/contexts/AuthContext/useAuth";
+import { auth } from "@/lib/firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import EmailField from "../../shared/EmailField/EmailField";
 import PasswordField from "../../shared/PasswordField/PasswordField";
 import CreateAccountButton from "./CreateAccountButton/CreateAccountButton";
 import NavigationLink from "@/components/ui/NavigationLink/NavigationLink";
-import {
-  signUpEmailValidation,
-  signUpPasswordValidation,
-} from "./AuthStep.utils";
+import FormErrorMessage from "@/components/shared/Form/FormErrorMessage/FormErrorMessage";
 
 /**
  * Renders the user authentication form step used by the SignUpForm component
@@ -19,6 +25,9 @@ export default function AuthStep({ nextStep }: AuthStepProps) {
 
   // Translation
   const { t } = useTranslation();
+
+  // Context
+  const { setUser } = useAuth();
 
   // React Hook Form: methods
   const methods = useForm<AuthenticationFields>();
@@ -30,19 +39,29 @@ export default function AuthStep({ nextStep }: AuthStepProps) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     watch,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = methods;
+
+  // State
+  const [submitError, setSubmitError] = useState<string>("");
 
   /**
    * Submits the authentication form
+   * @param data email and password fields
+   *
+   * If an error is catched, sets submitError state to display a form error message
    */
-  const onSubmit = () => {
-    try {
-      // TO DO: Add Firebase account creation
-      nextStep();
-    } catch (error) {
-      /*TO DO: redirect to error page*/
-    }
+  const onSubmit = (data: AuthenticationFields) => {
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+        nextStep();
+      })
+      .catch((error) => {
+        const errorMessage = getCreateUserErrorMessage(t, error);
+        setSubmitError(errorMessage);
+      });
   };
 
   return (
@@ -52,7 +71,7 @@ export default function AuthStep({ nextStep }: AuthStepProps) {
         className="mb-8
                   font-black 
                   text-center text-xl text-title
-                  lg:text-2xl"
+                  lg:text-left lg:text-2xl"
       >
         {t("forms.signUp.auth-step.title")}
       </h1>
@@ -60,14 +79,22 @@ export default function AuthStep({ nextStep }: AuthStepProps) {
       <FormProvider {...methods}>
         <form
           className="w-full"
-          onSubmit={methods.handleSubmit(onSubmit)}
+          onSubmit={methods.handleSubmit((data) => onSubmit(data))}
           noValidate
         >
           <EmailField validation={signUpEmailValidation} />
           <PasswordField validation={signUpPasswordValidation} />
 
-          <div className="sm:mx-auto sm:w-2/3 lg:w-full">
-            <CreateAccountButton />
+          <div
+            className="min-h-17.5 overflow-hidden sm:mx-auto sm:w-2/3 lg:w-full"
+            aria-live="assertive"
+            aria-atomic="true"
+          >
+            {submitError ? <FormErrorMessage message={submitError} /> : null}
+          </div>
+
+          <div className="mt-2 sm:mx-auto sm:w-2/3 lg:w-full">
+            <CreateAccountButton isSubmitting={isSubmitting} />
           </div>
         </form>
       </FormProvider>

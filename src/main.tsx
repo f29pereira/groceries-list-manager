b61e3b412b5db896a12148c65d5c089158b7b90c
Filipe Pereira <f29.pereira@gmail.com>
2026-09-10 16:52:25 +0100

    feat: Integrate AuthProvider and i18n into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,21 @@ import { createRoot } from "react-dom/client";
 import "@/assets/styles/globals.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+import "./lib/i18n/i18n";
 import ThemeProvider from "./contexts/ThemeContext/ThemeProvider";
 import LanguageProvider from "./contexts/LanguageContext/LanguageProvider";
-import "./lib/i18n/i18n";
+import AuthProvider from "./contexts/AuthContext/AuthProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {
-      <ThemeProvider>
-        <LanguageProvider>
-          <RouterProvider router={router} />
-        </LanguageProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <LanguageProvider>
+            <RouterProvider router={router} />
+          </LanguageProvider>
+        </ThemeProvider>
+      </AuthProvider>
     }
   </StrictMode>,
 );
